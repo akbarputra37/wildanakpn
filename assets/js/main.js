404a9c8c31b12a25328b1b7d2e01d9f72afeea0e
wildanakpn <wildanakpn@gmail.com>
2024-07-22 10:08:48 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// JavaScript to redirect the user automatically
+window.location.href = "https://www.akbarputra.xyz";
+
+// Simulate a mouse click:
+window.location.href = "https://www.akbarputra.xyz";
+
+// Simulate an HTTP redirect:
+window.location.replace("https://www.akbarputra.xyz");
+
+
+// similar behavior as an HTTP redirect
+window.location.replace("https://www.akbarputra.xyz");
+
+// similar behavior as clicking on a link
+window.location.href = "https://www.akbarputra.xyz";
